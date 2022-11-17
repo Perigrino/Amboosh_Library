c77723b0689b75b3f7865cee34051c0e708f1508
perigrinobruce <chasebruce1992@gmail.com>
2022-11-17 20:29:34 +0000

    Cleaned up variable names in all Services Files

--- Services/PublisherService.cs
+++ Services/PublisherService.cs
@@ -12,46 +12,46 @@ public class PublisherService
         _context = context;
     }
 
-    public void AddPublisher(PublisherVM publisher) //Adds a Publisher to db
+    public void AddPublisher(PublisherVM publisherObj) //Adds a Publisher to db
     {
-        var _publisher = new Publisher()
+        var publisher = new Publisher()
         {
-            Name = publisher.Name
+            Name = publisherObj.Name
         };
-        _context.Publishers.Add(_publisher);
+        _context.Publishers.Add(publisher);
         _context.SaveChanges();
     }
 
     public List<Publisher> GetAllPublisher() //Gets a list of all Publisher
     {
-        var allPublisher = _context.Publishers.ToList();
-        return allPublisher;
+        var publishers = _context.Publishers.ToList();
+        return publishers;
     }
 
     public Publisher GetPublisherById(int publisherId) //Gets Publisher by Id
     {
-        var publisherById = _context.Publishers.FirstOrDefault(n => n.Id == publisherId);
-        return publisherById;
+        var publisher = _context.Publishers.FirstOrDefault(n => n.Id == publisherId);
+        return publisher;
     }
 
-    public Publisher UpdatePublisherById(int publisherId, PublisherVM publisher) //Update a Publisher 
+    public Publisher UpdatePublisherById(int publisherId, PublisherVM publisherObj) //Update a Publisher 
     {
-        var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == publisherId);
-        if (_publisher != null)
+        var publisher = _context.Publishers.FirstOrDefault(n => n.Id == publisherId);
+        if (publisher != null)
         {
-            _publisher.Name = publisher.Name;
+            publisher.Name = publisherObj.Name;
 
             _context.SaveChanges();
         }
-        return _publisher;
+        return publisher;
     }
 
     public void DeleteById(int publishId) //Deletes a Publisher by Id
     {
-        var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == publishId);
-        if (_publisher != null)
+        var publisher = _context.Publishers.FirstOrDefault(n => n.Id == publishId);
+        if (publisher != null)
         {
-            _context.Publishers.Remove(_publisher);
+            _context.Publishers.Remove(publisher);
             _context.SaveChanges();
         }
     }
